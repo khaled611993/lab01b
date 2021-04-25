386bbb27f5c06972971e52a3da8e888a4de98c26
khaled611993 <khaledfa.93@gmail.com>
2021-04-25 15:38:07 +0300

    update

---
+++ app.js
@@ -0,0 +1,15 @@
+let yourName = prompt('please enter your name')
+console.log(yourName);
+//alert(yourName + 'is abeautiful name')
+
+let yourAge = prompt( 'please,can you tell me your age?')
+console.log(yourAge);
+
+let yourFavcolor=prompt('I am wondering what is your favorite color?')
+console.log(yourFavcolor);
+
+let x = prompt('Were my questions annoying?')
+console.log(x);
+
+
+alert(' yourname is ' + yourName + ' and yourAge is '+ yourAge +' and your  favorite  color is ' + yourFavcolor +'  Thanks for being so nice ');
